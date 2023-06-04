30fffc9f83c1fc68725dd0330886e1421c96ad1e
IXLLEGACYIXL <107197024+IXLLEGACYIXL@users.noreply.github.com>
2023-06-04 05:09:43 +0200

    First working version.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Stride.Core.Extensions;
+using Stride.Core.Shaders.Ast.Hlsl;
+using StrideSourceGened;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
+TestClass class1 = new TestClass();
+TestClass class2 = new TestClass()
+    {
+    Number = 1,
+    FancyClass = new TestClass()
+      
+        {
+        Number = 2,
+            FancyClass = new TestClass()
+            {
+                Number = 3,
+                Test = "Hello recursive"
+            }
+        }
+
+    };
+var x = GeneratedSerializerTestClass.WriteToDictionary(class1);
+var y = GeneratedSerializerTestClass.WriteToDictionary(class2);
+Console.WriteLine(x);
---
+++ TestClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using YamlDotNet.Serialization;
+
+namespace StrideSourceGened
+{
+    [DataContract]
+    public partial class TestClass
+    {
+        [DataMember]
+        public int Number { get; set; } = 10;
+        [DataMember]
+        public string Test { get; set; } = "hello mister";
+        [DataMember]
+        public TestClass FancyClass { get; set; }
+        public TestClass FancyClass2 { get; set; }
+        public void WriteToFile(TestClass objectToSerialize, string filePath)
+        {
+            
+            var serializer = new SerializerBuilder().Build();
+
+            var dict = new Dictionary<string, object>();
+            dict["$Type"] = typeof(TestClass).AssemblyQualifiedName;
+            var propertiesDictionary = new Dictionary<string, object>();
+            dict["$Attributes"] = propertiesDictionary;
+
+
+            using (var writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, dict);
+            }
+        }
+    }
+
+}
+
