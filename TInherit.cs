d5c6ff429eb40f4dc00ec89ead7fc014ea29afa7
IXLLEGACYIXL <107197024+IXLLEGACYIXL@users.noreply.github.com>
2023-06-08 14:06:48 +0200

    fix multi class file scoped namespace

--- TInherit.cs
+++ TInherit.cs
@@ -1,13 +1,21 @@
 ﻿
+using Stride.Core.Mathematics;
 using System.Runtime.Serialization;
 
 namespace StrideSourceGened;
 
-[DataContract]
-public class TInherit : TInherit2
-{
-    [DataMember]
-    public int FancyNumber { get; set; } = 10;
-}
+    [DataContract]
+    public class TInherit : TInherit2
+    {
+        [DataMember]
+        public int FancyNumber { get; set; } = 10;
+    }
 
+    [DataContract]
+    public partial class TInherit2
+    {
+        public int Nubmers { get; set; }
+        public Int2 Int2 { get; set; } = new Int2() { X = 1, Y = 2 };
+        public int Nubmers2 { get; set; }
+    }
 
--- TInherit2.cs
+++ TInherit2.cs
@@ -4,11 +4,6 @@
 namespace StrideSourceGened
 {
 
-    public partial class TInherit2
-    {
-        public int Nubmers { get; set; }
-        public Int2 Int2 { get; set; } = new Int2() { X = 1 ,Y = 2 };
-        public int Nubmers2 { get; set; }
-    }
+
 }
 
