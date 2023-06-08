35d5cfa92076a594f4b7e4f72c39a0488f30b588
IXLLEGACYIXL <107197024+IXLLEGACYIXL@users.noreply.github.com>
2023-06-08 14:38:34 +0200

    fix issue for normal scoped namespace with multiple classes

--- TInherit2.cs
+++ TInherit2.cs
@@ -1,8 +1,25 @@
-﻿using Stride.Core.Mathematics;
+﻿using Stride.Core;
+using Stride.Core.Mathematics;
 
 
 namespace StrideSourceGened
 {
+
+
+    [DataContract]
+    public class TInherit3 : TInherit2
+    {
+        [DataMember]
+        public int FancyNumber { get; set; } = 10;
+    }
+
+    [DataContract]
+    public partial class TInherit4
+    {
+        public int Nubmers { get; set; }
+        public Int2 Int2 { get; set; } = new Int2() { X = 1, Y = 2 };
+        public int Nubmers2 { get; set; }
+    }
 
 
 }
