ec6023efab12138fee632ba106375aafe614acd9
IXLLEGACYIXL <ixllegacy123@outlook.com>
2023-12-04 19:39:43 +0100

    apply new serializer

--- TInherit.cs
+++ TInherit.cs
@@ -4,9 +4,16 @@
 [System.Runtime.Serialization.DataContract]
 public class TInherit
 {
-    [System.Runtime.Serialization.DataMember]
     public int Nun { get; set; } = 10;
-
+    [System.Runtime.Serialization.IgnoreDataMember]
+    public int Ignore { get; set; } = 100;
+    public List<int> ints { get; set; } = new List<int>() { 1, 2, 3 };
+    public int[] intArray { get; set; } = { 1, 2, 3 };
+    public Dictionary<int, string> keyValuePairs { get; set; } = new Dictionary<int, string>()
+    {
+        [0] = "1",
+        [1] = "2",
+    };
     public object Clone()
     {
         
